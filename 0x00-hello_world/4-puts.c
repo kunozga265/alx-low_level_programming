7bc15011f840115711ab7e796e40db3ed717ecaa
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2023-02-16 12:30:28 -0800

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *Prints statement
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char Mystr[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(Mystr);
+
+	return (0);
+}
