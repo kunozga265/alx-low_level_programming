d32be62d836b0731e24c4e77e293f4c12e742ff8
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2023-02-17 14:02:09 -0800

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints numbers from 0 to 9
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
