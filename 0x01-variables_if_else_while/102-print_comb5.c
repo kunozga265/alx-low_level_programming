cdcb052ef8685a76f90a740e25b5d8a42971dbce
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2023-02-17 16:29:49 -0800

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints numbers from 0 to 9
+ * Separated by , and space
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i = 0;
+	int y, j, k;
+
+	while (i < 10)
+	{
+		for (y = 0; y < 9; y++)
+		{
+			for (k = y; k < 10; k++)
+			{
+				for (j = y + 1; j < 10; j++)
+				{
+					putchar(i + '0');
+					putchar(y + '0');
+					putchar(32);
+					putchar(k + '0');
+					putchar(j + '0');
+					if (i != 9 || y != 8 || k != 9 || j != 9)
+					{
+						putchar(',');
+						putchar(32);
+					}
+				}
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
