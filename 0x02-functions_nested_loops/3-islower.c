0ec58a326dccc60f7bb8b4ed21499111fa4286ef
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2023-02-21 15:37:13 -0800

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,25 +2,31 @@
 
 /**
  * _islower - function
+ * @c: number checked
  *
  * Description: Checks whether c is a lower alphabet character
  * Return: Nothing
  */
 int _islower(int c)
 {
-	int check=0, index =0;
-	char alphabet[26]="abcdefghijklmnopqrstuvwxy";
-	
+	int check = 0, index = 0;
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxy";
+
 	if (c >= 97 && c <= 122)
 	{
-		check=1;
-	}else
+		check = 1;
+	}
+	else if (c == 0)
+	{
+		check = 0;
+	}
+	else
 	{
 		while (index < 26)
 		{
-			if(c == alphabet[index])
+			if (c == alphabet[index])
 			{
-				check=1;
+				check = 1;
 			}
 			index++;
 		}
