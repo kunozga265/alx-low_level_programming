430bd7598a24bab5df3a34500876198444367b0a
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2023-02-21 17:32:03 -0800

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function
+ * @n: first number
+ * @m: second number
+ *
+ * Description: Determines sum
+ * Return: Sum
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
