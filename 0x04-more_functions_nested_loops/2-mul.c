b13516bd6bdf238f498d72fbbc489adb1b89f3ea
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2023-02-24 06:03:35 -0800

    3. The numbers speak for themselves mandatory

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,32 @@
 #include "main.h"
 
 /**
- * mul - multiplies two numbers together
- * @x: number
- * @y: number
+ * _isdigit - function
+ * @c: number checked
  *
- * Return: Product
+ * Description: Checks whether c is a digit
+ * between 0 and 9
+ * Return: 1 or 0
  */
-int mul(int x, int y)
+int _isdigit(int c)
 {
-	return (x * y);
+	int check = 0, index = 0;
+	char digits[10] = "0123456789";
+
+	if (c >= 0 && c <= 9)
+	{
+		check = 1;
+	}
+	else
+	{
+		while (index < 10)
+		{
+			if (c == digits[index])
+			{
+				check = 1;
+			}
+			index++;
+		}
+	}
+	return (check);
 }
