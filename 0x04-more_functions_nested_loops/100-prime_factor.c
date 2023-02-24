48a50af079d7b75142caef84d7b852694818603f
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2023-02-24 13:35:11 -0800

    11. The problem of distinguishing prime numbers f

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	long int n = 612852475143;
+
+	while (n % 2 == 0)
+	{
+		/* printf("2 "); */
+		n = n / 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			/* printf("%d ", i); */
+			n = n / i;
+		}
+	}
+	if (n > 2)
+	{
+		printf("%ld\n", n);
+	}
+	return (0);
+}
