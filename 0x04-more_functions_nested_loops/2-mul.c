b93e12e470eb0f7a5ebaeef48181af2d3860fed0
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2023-02-24 06:09:13 -0800

    2. Collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,32 +1,12 @@
 #include "main.h"
 
 /**
- * _isdigit - function
- * @c: number checked
- *
- * Description: Checks whether c is a digit
- * between 0 and 9
- * Return: 1 or 0
+ * mul - multiply two numbers
+ * @x: first number
+ * @y: second number
+ * Return: Product
  */
-int _isdigit(int c)
+int mul(int x, int y)
 {
-	int check = 0, index = 0;
-	char digits[10] = "0123456789";
-
-	if (c >= 0 && c <= 9)
-	{
-		check = 1;
-	}
-	else
-	{
-		while (index < 10)
-		{
-			if (c == digits[index])
-			{
-				check = 1;
-			}
-			index++;
-		}
-	}
-	return (check);
+	return (x * y);
 }
