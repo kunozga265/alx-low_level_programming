e9fc46ec231c0880f5bf41abcd884c33e6297e8f
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2023-02-27 14:08:07 -0800

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @s: pointer
+ * Return: Nothing
+ */
+void puts2(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}
