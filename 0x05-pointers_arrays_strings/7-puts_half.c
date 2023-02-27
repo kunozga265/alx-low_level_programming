7a33ae38552480ac44e4098e00230bec73793b06
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2023-02-27 14:27:27 -0800

    7. Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints last half
+ * @s: pointer
+ * Return: Nothing
+ */
+void puts_half(char *s)
+{
+	int i;
+	int j = 0;
+
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+	if (j % 2 == 0)
+	{
+		for (i = j / 2; i <= j; i++)
+		{
+			_putchar(s[i]);
+		}
+	}
+	else
+	{
+		for (i = (j - 1) / 2; i <= j; i++)
+		{
+			_putchar(s[i]);
+		}
+	}
+	_putchar('\n');
+}
